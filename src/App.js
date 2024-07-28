b628fea5df41dbe992ebb88813f849df4bb6f08a
SalemMohammed777 <salemalgony3000@gmail.com>
2024-07-28 22:46:35 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-function App() {
-  return <div>App.js file</div>;
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Html from "./pages/html";
+import Css from "./pages/css";
+import Javascript from "./pages/javascript";
 
+function App() {
+  return (
+  <div className="App">
+    <h1 style={{ color: "red" }}> welcome to React Router!</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/html" element={<Html />} />
+      <Route path="/css" element={<Css />} />
+      <Route path="/javascript" element={<Javascript />} />
+    </Routes>   
+  </div>
+  );
 }
 
 export default App;
---
+++ src/Component/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        Designed and developed by salemalgony3000@gmail.com
+        <span>🧡</span>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
