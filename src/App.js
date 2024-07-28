c9c10de92a6b4f7d123c6ee977fbe00ce98f1830
SalemMohammed777 <salemalgony3000@gmail.com>
2024-07-28 23:53:46 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import Javascript from "./pages/javascript";
 
 function App() {
   return (
-  <div className="App">
-    <h1 style={{ color: "red" }}> welcome to React Router!</h1>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/html" element={<Html />} />
-      <Route path="/css" element={<Css />} />
-      <Route path="/javascript" element={<Javascript />} />
-    </Routes>   
-  </div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/html" element={<Html />} />
+        <Route path="/css" element={<Css />} />
+        <Route path="/javascript" element={<Javascript />} />
+      </Routes>
+    </div>
   );
 }
 
